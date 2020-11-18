8fcbc8569e56ec69548daf57206135d3e8c2a7a1
Naren Sulegai <86naren@gmail.com>
2020-11-18 17:05:59 +0530

    Using redis for api

--- Backend/modules/index.js
+++ Backend/modules/index.js
@@ -1,5 +1,5 @@
 const {
-  Company, JobPosting,
+  Company, JobPosting, CompanySalary,
 } = require('../mongodb');
 
 module.exports = {
@@ -9,5 +9,21 @@ module.exports = {
     const company = await Company.findById(companyId);
     company.jobPostings.push(newJobPosting._id);
     return company.save();
+  },
+  getCompanyJobPosting: async (companyId) => {
+    console.log('getCompanyJobPosting');
+    const jobPostings = await JobPosting.find({ company: companyId });
+    const companySalaries = await CompanySalary.aggregate([
+      { $group: { _id: '$jobPosting', minBaseSalary: { $min: '$baseSalary' }, maxBaseSalary: { $max: '$baseSalary' } } },
+    ]);
+
+    const keyByJobPostingId = companySalaries
+      .reduce((m, s) => ({ ...m, [s._id]: s }), {});
+
+    return jobPostings.map((j) => {
+      const { minBaseSalary, maxBaseSalary } = keyByJobPostingId[j._id]
+      || { minBaseSalary: null, maxBaseSalary: null };
+      return { ...j.toJSON(), minBaseSalary, maxBaseSalary };
+    });
   },
 };
