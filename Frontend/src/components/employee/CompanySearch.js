7184e509ea75776fbe864817b3211d3460a49288
shiva-lab <shiva.pandey@sjsu.edu>
2020-11-24 10:47:01 -0800

    addded pagination for first index in company search

--- Frontend/src/components/employee/CompanySearch.js
+++ Frontend/src/components/employee/CompanySearch.js
@@ -2,19 +2,36 @@ import React, {
   createRef, useEffect, useState,
 } from 'react';
 import { searchCompany } from '../../util/fetch/api';
+import Pagination from '../../util/paginate/Pagination'
 
 const CompanySearch = () => {
   const [companies, setCompanies] = useState([]);
+
+  const [loading, setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [companiesPerPage] = useState(2);
+
   const searchTextRef = createRef();
   // TODO useCallback
   const handleOnSearch = async () => {
+    console.log("I am running too in handle")
     const text = searchTextRef.current.value;
     setCompanies(await searchCompany(text));
   };
 
   useEffect(() => {
+    console.log("I am running in useeffect")
     handleOnSearch();
   }, []);
+
+  const indexOfLastCompanies = currentPage * companiesPerPage;
+  const indexOfFirstcompanies = indexOfLastCompanies - companiesPerPage;
+  const currentCompanies = companies.slice(indexOfFirstcompanies, indexOfLastCompanies);
+  console.log("currentCompanies",currentCompanies)
+
+   // Change page
+   const paginate = pageNumber => setCurrentPage(pageNumber);
+
 
   return (
     <div className="row">
@@ -26,8 +43,8 @@ const CompanySearch = () => {
         </div>
 
         <div className="mt-3">
-          {companies.length === 0 && <div>No companies to show</div>}
-          {companies.map((c) => {
+          {currentCompanies.length === 0 && <div>No companies to show</div>}
+          {currentCompanies.map((c) => {
             return (
               <div key={c._id} className="card mb-3">
                 <div className="card-body">
@@ -61,6 +78,14 @@ const CompanySearch = () => {
             );
           })}
         </div>
+
+
+        <Pagination
+        companiesPerPage={companiesPerPage}
+        totalCompanies={companies.length}
+        paginate={paginate}
+      />
+
 
       </div>
     </div>
