353a9432820cfa921741a8b1f864861ff111fa99
Akash Singhal <akash.singhal67@yahoo.com>
2020-11-30 09:21:52 +0530

    added apis for fetching reviews by company and status - implementation file

--- Backend/apiHandler/admin/index.js
+++ Backend/apiHandler/admin/index.js
@@ -1,37 +1,41 @@
-const { Review, CompanyPhoto } = require('../../mongodb');
-const { err } = require('../util');
+const { Review, CompanyPhoto } = require("../../mongodb");
+const { err } = require("../util");
 
 module.exports = {
   getReviews: async (req, res) => {
-    const reviews = await Review.find(
-      {
-        status: req.params.status,
-      },
-    )
-      .populate('company', 'name')
-      .populate('employee', 'email');
+    const reviews = await Review.find({
+      status: req.params.status,
+    })
+      .populate("company", "name")
+      .populate("employee", "email");
+    res.json(reviews);
+  },
+  getReviewsByCompanyIdAndStatus: async (req, res) => {
+    const reviews = await Review.find({
+      status: req.params.status, company: req.params.id
+    })
+      .populate("company", "name")
+      .populate("employee", "email");
     res.json(reviews);
   },
   getPrivatePhotos: async (req, res) => {
-    const images = await CompanyPhoto.find(
-      {
-        status: req.params.status,
-      },
-    )
-      .populate('company', 'name')
-      .populate('employee', 'email');
+    const images = await CompanyPhoto.find({
+      status: req.params.status,
+    })
+      .populate("company", "name")
+      .populate("employee", "email");
     res.json(images);
   },
   approveReview: async (req, res) => {
     const review = await Review.findById(req.params.id);
-    if (req.body.status === 'approved') review.status = 'approved';
-    else review.status = 'rejected';
+    if (req.body.status === "approved") review.status = "approved";
+    else review.status = "rejected";
     res.json(await review.save());
   },
   approvePhoto: async (req, res) => {
     const photo = await CompanyPhoto.findById(req.params.id);
-    if (req.body.status === 'approved') photo.status = 'approved';
-    else photo.status = 'rejected';
+    if (req.body.status === "approved") photo.status = "approved";
+    else photo.status = "rejected";
     res.json(await photo.save());
   },
 };
